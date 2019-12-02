4b6d85b840b4becd8db94488874ca8af93b82db3
modasser <imtishad@gmail.com>
2019-12-02 20:12:05 +0600

    Add disqus

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -35,7 +35,7 @@ module.exports = {
         link: "/pages/about/"
       }
     ],
-    // disqus: "https-wonderful-pare-b8e3cb-netlify-com", // if you want to incorporate Disqus for comments replace this value else just get rid of it
+    disqus: "sleepy-montalcini-0ec249.netlify.com", // if you want to incorporate Disqus for comments replace this value else just get rid of it
     socialShare: true, // enables social share
     socialShareNetworks: ["facebook", "twitter"], // required for social share plugin
     footer: {
