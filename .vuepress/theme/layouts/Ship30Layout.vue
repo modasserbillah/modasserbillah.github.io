1fb6b429977ff960ae49cb37bb622d1349e8a22f
Modasser Billah <imtishad@gmail.com>
2021-12-12 00:42:06 +0600

    minor mods

--- .vuepress/theme/layouts/Ship30Layout.vue
+++ .vuepress/theme/layouts/Ship30Layout.vue
@@ -2,9 +2,9 @@
   <div>
     <div>
         <h1>Ship30for30 Atomic Essays</h1>
-        <p style="font-size: 20px;"> I onboarded the online writing voyage <a href="http://ship30for30.com/" target="_blank">Ship30for30</a> this August to get better at writing.
+        <p style="font-size: 20px;"> I onboarded the online writing voyage <a href="http://ship30for30.com/" target="_blank">Ship30for30</a> in August'21 to get better at writing.
             Although I got shipwrecked after Day 22, it was a great experience. I learned a lot of new stuff,
-            met some new people and explored the world of online writng up close.
+            met some new people and explored the world of online writing up-close.
 
             This page contains all my atomic essays from that journey in both image and text version.
         </p>
